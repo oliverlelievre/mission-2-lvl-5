1dcaa048c9bfc8fba246e96f48422907501dd85e
oliverlelievre <oliverl@missionreadyhq.com>
2023-05-01 17:18:17 +1200

    installed react router and finished navbar

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,28 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+export default function Navbar() {
+  const path = window.location.pathname;
+  return (
+    <nav className="nav">
+      <Link to="/" className="nav__logo">
+        Turners Cars
+      </Link>
+      <ul>
+        <CustomLink to="/pricing">Pricing</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive === to ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
