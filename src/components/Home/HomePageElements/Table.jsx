f8f98d857e920c4ed663972f112461dde7748d8d
oliverlelievre <oliverl@missionreadyhq.com>
2023-05-02 19:10:30 +1200

    updates

--- src/components/Home/HomePageElements/Table.jsx
+++ src/components/Home/HomePageElements/Table.jsx
@@ -13,7 +13,7 @@ export default class Table extends Component {
     componentDidMount() {
             const options = {
                 method: 'GET',
-                url: 'https://car-data.p.rapidapi.com/cars',
+                url: `https://car-data.p.rapidapi.com/cars`,
                 params: {limit: '10', page: '0'},
                 headers: {
                   'X-RapidAPI-Key': '1a04c9bf10mshc0ee6b403e14ab8p1e0a98jsnf29149217dba',
